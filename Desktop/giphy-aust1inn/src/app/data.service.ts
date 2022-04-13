8058f5e615f193fee62f5db0e80fffb25aadcd08
aust1inn <austinotieno364@gmail.com>
2022-04-13 22:35:25 +0300

    commit

---
+++ Desktop/giphy-aust1inn/src/app/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http:HttpClient) { }
+
+  getTrendingGifs () {
+    return this.http.get('api.giphy.com/v1/gifs/trending?api_key=% $[environment.giphyApiKey]&limit=50')
+  }
+}
